8ac6720c4d2b6eaa0ae64c922a564a77d80232be
Lucas-Mdeiros <lucas_mdeiros@outlook.com>
2025-03-21 15:22:42 -0300

    Variaveis e tratamento de strings

--- primeiroprojeto/src/Main.java
+++ primeiroprojeto/src/Main.java
@@ -1,11 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Esse é o Screen Match");
         System.out.println("Filme: Top Gun: Maverick");
 
         int year = 2022;
-
+        System.out.println("Ano de lançamento: " + year);
+        float score = 8.2F;
+        System.out.println("Nota do filme: " + score);
+        String name = "Top Gun: Maverick";
+        double avg = (9.8 + 7.7 + 8.6) / 3;
+        int rating = (int) (avg/2);
+        System.out.println(avg);
+        System.out.println(rating);
     }
 }
