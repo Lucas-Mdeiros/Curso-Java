501b6c653c321cb329b3b9d29044c56fbdf33a03
Lucas-Mdeiros <lucas_mdeiros@outlook.com>
2025-03-21 14:57:12 -0300

    Inicio Curso Java

---
+++ primeiroprojeto/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int year = 2022;
+
+    }
+}
