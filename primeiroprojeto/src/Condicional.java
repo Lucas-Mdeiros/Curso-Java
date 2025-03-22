95592cfe5665ff37145fcb97a0fc3127b4834cfd
Lucas-Mdeiros <lucas_mdeiros@outlook.com>
2025-03-22 09:20:34 -0300

    Criar classe Condicional

---
+++ primeiroprojeto/src/Condicional.java
@@ -0,0 +1,14 @@
+public class Condicional {
+    public static void main(String[] args) {
+        int age = 2022;
+        boolean addInPlan = true;
+        double rating = 8.1;
+        String typePlan = "plus";
+
+        if(age >= 2022){
+            System.out.println("Filme lancamento que os clientes estao curtindo");
+        }else{
+            System.out.println("Filme retro");
+        }
+    }
+}
