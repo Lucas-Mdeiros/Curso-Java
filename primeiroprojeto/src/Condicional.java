0998b30b7c6ab1d308d5b63bb0b260a2f5fddad5
Lucas-Mdeiros <lucas_mdeiros@outlook.com>
2025-03-22 15:30:08 -0300

    Utilizando lacos de repeticao

--- primeiroprojeto/src/Condicional.java
+++ primeiroprojeto/src/Condicional.java
@@ -1,6 +1,6 @@
 public class Condicional {
     public static void main(String[] args) {
-        int age = 2022;
+        int age = 2001;
         boolean addInPlan = true;
         double rating = 8.1;
         String typePlan = "plus";
@@ -9,6 +9,12 @@ public class Condicional {
             System.out.println("Filme lancamento que os clientes estao curtindo");
         }else{
             System.out.println("Filme retro");
+        }
+
+        if(addInPlan || typePlan.equals("plus")){
+            System.out.println("Filme Liberado");
+        }else{
+            System.out.println("Deve pagar a locacao");
         }
     }
 }
