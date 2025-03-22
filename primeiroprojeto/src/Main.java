5da36d0fae6d006b8f89bd44cc2aaeb3ff97a14b
Lucas-Mdeiros <lucas_mdeiros@outlook.com>
2025-03-22 08:55:35 -0300

    Desafios concluidos

--- primeiroprojeto/src/Main.java
+++ primeiroprojeto/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        //Principal
+        /*
         System.out.println("Esse é o Screen Match");
         System.out.println("Filme: Top Gun: Maverick");
 
@@ -12,8 +14,37 @@ public class Main {
         int rating = (int) (avg/2);
         System.out.println(avg);
         System.out.println(rating);
+        */
+        //Atividades
+        /* Atividades
         double tempCelsius = 32;
         int tempFahrenheit = (int)(tempCelsius * 1.8) + 32;
-        System.out.println("Temperatura em Fahrenheit é: " +tempFahrenheit + "F");
+        System.out.println("Temperatura em Fahrenheit é: " +tempFahrenheit + "F"); */
+        //Desafios
+        double nota1 = 9.8;
+        double nota2 = 7.6;
+        double result = (nota1 + nota2) / 2;
+        System.out.println(result);
+
+        int resultInt = (int) result;
+        System.out.println(resultInt);
+
+        char caracter = '!';
+        String sentence = "O dia esta chuvoso";
+        System.out.println(sentence + caracter);
+
+        double priceProduct = 349.99;
+        int quantity= 8;
+        System.out.println("O preco do produto e: " + priceProduct + " com " + quantity + " e o valor final e de " + (priceProduct * quantity));
+
+        double valueInDollars = 150.50;
+        double conversionForReal = valueInDollars * 5.76;
+        System.out.println(conversionForReal);
+
+
+        double priceOriginal = 550;
+        double percentDiscount = 10;
+        System.out.println((priceOriginal - (priceOriginal * (percentDiscount / 100))));
+
     }
 }
