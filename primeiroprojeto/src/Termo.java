ae9d3109d6d4224c39648637b9c635aec6317398
Lucas-Mdeiros <lucas_mdeiros@outlook.com>
2025-03-23 10:38:29 -0300

    Jogo Termo

---
+++ primeiroprojeto/src/Termo.java
@@ -0,0 +1,31 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class Termo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String wordSecret = "teste";
+        boolean truFal = true;
+        String word = "";
+        char[] aList = new char[5];
+        while(truFal){
+            System.out.println("");
+            System.out.println("Digite uma palavra: ");
+            word = sc.nextLine();
+            for(int i = 0; i < 5; i++){
+                if(wordSecret.charAt(i) == word.charAt(i)){
+                    aList[i]= word.charAt(i);
+                }else{
+                    aList[i] = '-';
+                }
+            }
+            for(int i = 0; i < aList.length; i++) {
+                System.out.print(aList[i]);
+            }
+            if(word.equals(wordSecret)){
+                System.out.println("\n Voce acertou a palavra");
+                break;
+            }
+        }
+    }
+}
